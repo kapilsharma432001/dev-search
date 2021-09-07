7aea1daa5952764d7f85c598de657e9a7d1947f8
FIRST_NAME LAST_NAME <sharmakapil432001@gmail.com>
2021-09-07 17:34:29 +0530

    Added dev-search on github

---
+++ projects/forms.py
@@ -0,0 +1,9 @@
+from django.db.models.base import Model
+from django.forms import ModelForm
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']

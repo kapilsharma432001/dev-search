343d04c53ef6047eb77b23ee67cca5951f77a46b
FIRST_NAME LAST_NAME <sharmakapil432001@gmail.com>
2021-09-08 17:36:52 +0530

    Working with static files on Django

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-vmh8*298x)!l1t$3jmx%ss2u5r!-$ojqx7gg2%&^m^&-+v-17_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -32,6 +32,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +123,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
